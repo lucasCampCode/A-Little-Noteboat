97f19cf86fbf1bf700d18f3b6ac45ac6f676f56c
Joseph Peña <jgpena2888@gmail.com>
2021-05-03 16:24:31 -0500

    Made HealthBehaviour script

---
+++ Assets/Scripts/Gameplay/HealthBehaviour.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthBehaviour : MonoBehaviour
+{
+    [SerializeField]
+    private float _health;
+
+    public float Health
+    {
+        get { return _health; }
+    }
+
+    /// <summary>
+    /// Subtracts the given damage value from health
+    /// </summary>
+    /// <param name="damage"></param>
+    public void takeDamage(float damage)
+    {
+        _health -= damage;
+
+        if (_health <= 0)
+            _health = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (_health <= 0)
+            gameObject.SetActive(false);
+    }
+}
