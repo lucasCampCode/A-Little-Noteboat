e62d65297b2dd72ad8ff721be3a4352211c375cf
Joseph Peña <jgpena2888@gmail.com>
2021-05-07 09:45:39 -0500

    Made GameManager script;
    Made ScoreCounter script for later UI use.

---
+++ Assets/Scripts/Gameplay/GameManagerBehaviour.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public delegate void GameEvent();
+
+public class GameManagerBehaviour : MonoBehaviour
+{
+    public static GameEvent onGameOver;
+
+    [SerializeField]
+    private HealthBehaviour _playerHealth;
+
+    [SerializeField]
+    private GameObject _gameOverScreen;
+
+    private static bool _gameOver = false;
+
+    //Keeps track of the players score
+    private float _score;
+
+    public static bool GameOver
+    {
+        get { return _gameOver; }
+    }
+
+    public float Score
+    {
+        get { return _score; }
+        set { _score = value; }
+    }
+
+    public void RestartGame()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.Score += 1;
+
+        _gameOver = _playerHealth.Health <= 0;
+
+        _gameOverScreen.SetActive(_gameOver);
+    }
+}
