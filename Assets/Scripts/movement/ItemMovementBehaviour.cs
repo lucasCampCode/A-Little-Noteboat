c4f4608217dfe7a17caa9035c31cc6c7c7643847
JosephPena1 <jgpena2888@gmail.com>
2021-05-10 16:34:56 -0500

    Removed unused code in ItemMovement;

--- Assets/Scripts/movement/ItemMovementBehaviour.cs
+++ Assets/Scripts/movement/ItemMovementBehaviour.cs
@@ -7,12 +7,6 @@ public class ItemMovementBehaviour : MonoBehaviour
     [SerializeField]
     private float _moveSpeed;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
     // Update is called once per frame
     void Update()
     {
