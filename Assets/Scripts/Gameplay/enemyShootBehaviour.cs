fece6661c131de0b750f96d7c7241256c4e016f0
lucas <lcampbell0815@gmail.com>
2021-05-10 14:23:33 -0500

    updated player movement and shooting to be handled in input script

---
+++ Assets/Scripts/Gameplay/enemyShootBehaviour.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemyShootBehaviour : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject _target;
+    [SerializeField]
+    private BulletEmitterBehaviour _bulletEmitter;
+    [SerializeField]
+    private float _speed = 5;
+    [SerializeField]
+    private float _timePerShoot = 2;
+
+    private float _time;
+
+    public GameObject Target
+    {
+        get { return _target; }
+        set { _target = value; }
+    }
+
+    void FixedUpdate()
+    {
+        Vector3 direction = (_target.transform.position - transform.position).normalized;
+
+        _time += Time.deltaTime;
+        if (_time > _timePerShoot)
+        {
+            _bulletEmitter.Fire(direction * _speed);
+            _time = 0;
+        }
+    }
+}
