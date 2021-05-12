0da68c754a7fad8ba20d02d0b9bca4012c8fff30
Judeous <judeous@tutanota.com>
2021-05-12 10:58:53 -0500

    Spawners will now allow enemies to go to the final items in arrays
    Also removed the example camera controller since it was unused

--- Assets/Scripts/Gameplay/SpawnerBehavior.cs
+++ Assets/Scripts/Gameplay/SpawnerBehavior.cs
@@ -16,7 +16,7 @@ public class SpawnerBehavior : MonoBehaviour
     [Tooltip("The time between waves of enemies")]
     [SerializeField] private float _timeBetweenWaves;
     [Tooltip("How long the spawner will spawn enemies more often")]
-    private float _waveTime = 4;
+    private float _waveDuration = 4;
     [Tooltip("Time since the most recent wave started")]
     private float _timeSinceWaveStart;
     [Tooltip("Time since the most recent wave ended")]
@@ -40,51 +40,80 @@ public class SpawnerBehavior : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        _timeBetweenSpawns = _calmSpawnTime;
         StartCoroutine(SpawnObjects());
     }
 
     private void Update()
     {
+        //If in a wave
         if (_inWave)
         {
             _timeSinceWaveStart += Time.deltaTime;
             //If the time since the wave started is less than the time that waves last
-            if (_timeSinceWaveStart < _waveTime)
-                _timeBetweenSpawns = _waveSpawnTime;
-            else
+            if (_timeSinceWaveStart >= _waveDuration)
             {
                 //Set inWave to be false
                 _inWave = false;
+                //Reset timeSinceWaveEnd
                 _timeSinceWaveEnd = 0;
+                //Set the time between spawns to be that of the calm spawn time
+                _timeBetweenSpawns = _calmSpawnTime;
             }
         }
+        //If not in a wave
         else
         {
             _timeSinceWaveEnd += Time.deltaTime;
             //If the time since the wave ended is less than the time between waves
-            if (_timeSinceWaveEnd < _timeBetweenWaves)
-                _timeBetweenSpawns = _calmSpawnTime;
-            else
+            if (_timeSinceWaveEnd >= _timeBetweenWaves)
             {
                 //Set inWave to be true
                 _inWave = true;
+                //Reset timeSinceWaveStart
                 _timeSinceWaveStart = 0;
+                //Set the time between spawns to be that of the wave spawn time
+                _timeBetweenSpawns = _waveSpawnTime;
             }
         }
     }
 
     public IEnumerator SpawnObjects()
     {
-        while(_canSpawn)
+        while (_canSpawn)
         {
             //Create a new enemy
             GameObject spawnedEnemy = Instantiate(_spawn, transform.position, new Quaternion());
 
-            //Set the enemy's loop positions, sit spot, and exit spots to random ones in the arrays
-            spawnedEnemy.GetComponent<EnemyMovementBehavior>().Loop1 = _loopPositions[Random.Range(0, _loopPositions.Length - 1)];
-            spawnedEnemy.GetComponent<EnemyMovementBehavior>().SitSpot = _sitSpots[Random.Range(0, _sitSpots.Length - 1)];
-            spawnedEnemy.GetComponent<EnemyMovementBehavior>().Loop2 = _loopPositions[Random.Range(0, _loopPositions.Length - 1)];
-            spawnedEnemy.GetComponent<EnemyMovementBehavior>().ExitSpot = _exitSpots[Random.Range(0, _exitSpots.Length - 1)];
+            //If only one loop position exists
+            if (_loopPositions.Length == 1)
+            {
+                //Set both loop positions to be the one position
+                spawnedEnemy.GetComponent<EnemyMovementBehavior>().Loop1 = _loopPositions[0];
+                spawnedEnemy.GetComponent<EnemyMovementBehavior>().Loop2 = _loopPositions[0];
+            }
+            else
+            {
+                //Set random loop positions
+                spawnedEnemy.GetComponent<EnemyMovementBehavior>().Loop1 = _loopPositions[Random.Range(0, _loopPositions.Length)];
+                spawnedEnemy.GetComponent<EnemyMovementBehavior>().Loop2 = _loopPositions[Random.Range(0, _loopPositions.Length)];
+            }
+
+            //If only one sit spot exists
+            if (_sitSpots.Length == 1)
+                //Set the spawn's sit spot to be that one spot
+                spawnedEnemy.GetComponent<EnemyMovementBehavior>().Loop1 = _sitSpots[0];
+            else
+                //Set a random sit spot
+                spawnedEnemy.GetComponent<EnemyMovementBehavior>().SitSpot = _sitSpots[Random.Range(0, _sitSpots.Length)];
+
+            //If only one exit spot exists
+            if (_exitSpots.Length == 1)
+                //Set the spawn's exit spot to be that one exit spot
+                spawnedEnemy.GetComponent<EnemyMovementBehavior>().ExitSpot = _exitSpots[0];
+            else
+                //Set a random exit spot
+                spawnedEnemy.GetComponent<EnemyMovementBehavior>().ExitSpot = _exitSpots[Random.Range(0, _exitSpots.Length)];
 
             //Set the enemy's shoot behavior's target to be the target the spawner was given
 
