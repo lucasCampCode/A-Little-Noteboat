045e8cf66f4f34ec773d20f1a89120fa2cfe461f
Judeous <judeous@tutanota.com>
2021-05-12 14:36:47 -0500

    Imported Event System Package (With Sample Scene)

---
+++ Assets/Scripts/CubeBehavior.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeBehavior : MonoBehaviour
+{
+    public void ToggleCube()
+    {
+        gameObject.SetActive(!gameObject.activeSelf);
+    }
+}
