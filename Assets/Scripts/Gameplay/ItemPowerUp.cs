08860b566d6132b50538d62b3f5732dae56de903
Joseph Pena <jgpena2888@gmail.com>
2021-05-19 15:35:57 -0500

    Made Scriptable PowerUp;

---
+++ Assets/Scripts/Gameplay/ItemPowerUp.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New PowerUp", menuName = "GamePlay/PowerUp")]
+public class ItemPowerUp : ScriptableObject
+{
+    public float rapidFire;
+}
