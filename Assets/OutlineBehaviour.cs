cfd59daf7310ad26dbc87cddb5eb88dc36c34be9
JosephPena1 <jgpena2888@gmail.com>
2021-05-28 11:09:39 -0500

    Made Outline script for toon shader;

---
+++ Assets/OutlineBehaviour.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OutlineBehaviour : MonoBehaviour
+{
+    [SerializeField] private Material outlineMaterial;
+    [SerializeField] private float outlineScaleFactor;
+    [SerializeField] private Color outlineColor;
+    private Renderer outlineRenderer;
+
+    void Start()
+    {
+        outlineRenderer = CreateOutline(outlineMaterial, outlineScaleFactor, outlineColor);
+        outlineRenderer.enabled = true;
+    }
+
+    Renderer CreateOutline(Material outlineMat, float scaleFactor, Color color)
+    {
+
+        GameObject outlineObject = Instantiate(this.gameObject, transform.position, transform.rotation, transform);
+        Renderer rend = outlineObject.GetComponent<Renderer>();
+        rend.material = outlineMat;
+        rend.material.SetColor("_OutlineColor", color);
+        rend.material.SetFloat("_ScaleFactor", scaleFactor);
+        rend.shadowCastingMode = UnityEngine.Rendering.ShadowCastingMode.Off;
+        outlineObject.GetComponent<OutlineBehaviour>().enabled = false;
+        outlineObject.GetComponent<Collider>().enabled = false;
+        rend.enabled = false;
+
+        return rend;
+    }
+
+    private void OnMouseOver()
+    {
+        transform.Rotate(Vector3.up, 1f, Space.World);
+    }
+}
