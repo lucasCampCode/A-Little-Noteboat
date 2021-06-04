fcce6846da6810b46715d360c6faa9d9824618bc
JosephPena1 <jgpena2888@gmail.com>
2021-06-04 10:32:06 -0500

    Modified KIllPlaneBehaviour; made obstacle prefabs;
    Made obstacles move down, damage the player, and get destroyed by killplane.

--- Assets/Scripts/Gameplay/KillPlaneBehaviour.cs
+++ Assets/Scripts/Gameplay/KillPlaneBehaviour.cs
@@ -7,7 +7,7 @@ public class KillPlaneBehaviour : MonoBehaviour
     private void OnTriggerEnter(Collider other)
     {
         //Checks if the other object is has an "Enemy" or "Item" tag
-        if (other.CompareTag("Enemy") || other.CompareTag("Item"))
+        if (other.CompareTag("Enemy") || other.CompareTag("Item") || other.CompareTag("Obstacle"))
             //If true, the object is deleted
             Destroy(other.gameObject);
     }
